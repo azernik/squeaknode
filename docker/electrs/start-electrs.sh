f775a83252e2e9037016136f9a9addabae8bf588
yzernik <yzernik@gmail.com>
2020-07-19 14:23:50 -0700

    Add docker container for electrs electrum in itest

---
+++ docker/electrs/start-electrs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# exit from script if error was raised.
+set -e
+
+# Add btcd's RPC TLS certificate to system Certificate Authority list.	exec runsqueak \
+cp /rpc/rpc.cert /usr/share/ca-certificates/btcd.crt
+echo btcd.crt >> /etc/ca-certificates.conf
+update-ca-certificates
+
+# To make python requests use the system ca-certificates bundle, it
+# needs to be told to use it over its own embedded bundle
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
+exec electrs -vvvv --timestamp --db-dir /home/user/db
